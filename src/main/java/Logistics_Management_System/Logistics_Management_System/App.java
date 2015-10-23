fa2ab82159365c8e3d4366c0dcc5d2837102b7b4
wongchifong <wongchifong0223@sina.com>
2015-10-23 17:41:42 +0800

    Let's begin

---
+++ src/main/java/Logistics_Management_System/Logistics_Management_System/App.java
@@ -0,0 +1,13 @@
+package Logistics_Management_System.Logistics_Management_System;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
