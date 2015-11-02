5c3efa89ac91bc849e7de93d7e08aea9bbe24a89
Vacity <zrui14@software.nju.edu.cn>
2015-11-02 22:00:46 +0800

    随便改改，看看你们能不能看到！

--- src/main/java/businesslogic/generalmanagerbl/AccountQueryImpl.java
+++ src/main/java/businesslogic/generalmanagerbl/AccountQueryImpl.java
@@ -1,12 +1,14 @@
 package businesslogic.generalmanagerbl;
 
+import dataservice.otherdataservice.FinanceService;
 import vo.generalmanagervo.Accountvo;
 import blservice.generalmanagerblservice.AccountQueryService;
 
 public class AccountQueryImpl implements AccountQueryService {
       
 	public Accountvo showAccount(){
+		FinanceService fs = new FinanceService();
 		System.out.println("显示银行账户信息");
 		return null;
 	}
-}
+} 
